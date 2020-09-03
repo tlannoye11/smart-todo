ed1e0aa891bbd9034384024a4f9cc847f3c96bb7
Travis Lannoye <travis.lannoye@gmail.com>
2020-09-02 22:29:42 -0500

    Completed Part 1

---
+++ src/components/todos-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class TodosList extends Component {
+    render() {
+        return (
+            <div>
+                <p>List of things Todo!</p>
+            </div>
+        )
+    }
+}
